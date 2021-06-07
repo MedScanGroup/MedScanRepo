adbf321e60d512ca8c6f3c57480b932d5377efb1
AndreaLizeth <liz-z96@hotmal.com>
2021-06-07 16:09:51 -0500

    Commit inicial

---
+++ QRApp/QRApp/QRApp/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+using Xamarin.Forms.Xaml;
+using Xamarin.Forms;
+
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+[assembly: ExportFont("Net-Baliung.ttf")]
+[assembly: ExportFont("Net-PreproductionDemoRegular.ttf")]
+[assembly: ExportFont("Net-Royal-Signature-ttf.ttf")]
+[assembly: ExportFont("Brazilian.ttf")]
+[assembly: ExportFont("MiraculousChristmas.ttf")]
+[assembly: ExportFont("SweetieBunny.ttf")]
